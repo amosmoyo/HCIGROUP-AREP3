489845bee43e698a8dbb77b022167a524430bfbc
amosmoyo <amosmoyo5300@gmail.com>
2019-10-20 10:14:25 +0300

    amos moyo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'HCIGROUP-A'`, () => {
+  it(`should have as title 'AngularProject'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('HCIGROUP-A');
+    expect(app.title).toEqual('AngularProject');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('HCIGROUP-A app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('AngularProject app is running!');
   });
 });
